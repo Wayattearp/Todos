14af5102a481cb65dd2abb327a816cf75eb08967
Wayattearp <iason148348@gmail.com>
2020-12-20 17:33:02 -0500

    Add store and initial state for testing

--- frontend/reducers/todo_reducers.js
+++ frontend/reducers/todo_reducers.js
@@ -1,4 +1,21 @@
-const todosReducer = (state = {}, action) => {
+
+const initialState = {
+    1: {
+        id: 1,
+        title: "wash car",
+        body: "with soap",
+        done: false
+    },
+    2: {
+        id: 2,
+        title: "wash dog",
+        body: "with shampoo",
+        done: true
+    }
+};
+
+
+const todosReducer = (state = initialState, action) => {
     Object.freeze(state);
     switch (action.type) {
         default:

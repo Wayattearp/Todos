9f71d8d9c77f3792dfae1eece9590a0f24111f7c
Wayattearp <iason148348@gmail.com>
2020-12-21 13:22:21 -0500

    Add provider to access store from props

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import App from './app'
 
-const Root = () => (
-        <h1>Todos</h1>
+//destructuring store from props.store
+const Root = ({ store }) => (
+        <Provider store={store}>
+                <App />
+        </Provider>
 )
 
 
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from './components/root';
 import configureStore from './store/store';
-import App from './components/app';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<App />, root)
+    ReactDOM.render(<Root />, root)
 });
 
 const store = configureStore();

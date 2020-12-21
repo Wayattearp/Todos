15db5c2ef73f88d03f2707696a0d164a0a813ba0
Wayattearp <iason148348@gmail.com>
2020-12-21 13:16:13 -0500

    Set up app component

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from './components/root';
 import configureStore from './store/store';
+import App from './components/app';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<Root />, root)
+    ReactDOM.render(<App />, root)
 });
 
 const store = configureStore();

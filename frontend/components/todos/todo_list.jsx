aaccb978ea5c8d63809f29a70f253f2f50999d8b
Wayattearp <iason148348@gmail.com>
2020-12-21 14:13:51 -0500

    set up todoList Container and todoList presentational components

---
+++ frontend/components/todos/todo_list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <h3>Todo List goes here</h3>
+    }
+}
+
+export default TodoList;

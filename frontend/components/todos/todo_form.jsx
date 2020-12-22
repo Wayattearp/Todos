4dd53927eb6cb5066b138401998860f4411096bf
Wayattearp <iason148348@gmail.com>
2020-12-21 19:43:00 -0500

    Build todo form

---
+++ frontend/components/todos/todo_form.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: "",
+            body: "",
+            done: false
+        };
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    update(property) {
+        return e => this.setState({ [property]: e.target.value });
+    }
+
+    handleSave(event) {
+        event.preventDefault();
+        const todo = Object.assign({}, this.state);
+        this.props.receiveTodo(todo);
+        this.setState({
+            title: "",
+            body: ""
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSave}>
+                    <label >Title:
+                    <input placeholder="get it done" value={this.state.title}
+                            onChange={this.update('title')} required />
+                    </label>
+
+                    <label >Body:
+                    <textarea placeholder="no excuses" value={this.state.body}
+                            onChange={this.update('body')} cols='20' rows='5' required />
+                    </label>
+                    <button> I will do! </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default TodoForm;
--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import TodoListItem from './todo_list_item'
+import TodoListItem from './todo_list_item';
+import TodoForm from './todo_form'
+import { receiveTodo } from '../../actions/todo_actions';
 
 class TodoList extends React.Component {
 
     render() {
-        const {todos } = this.props;
-       const todoItems = todos.map(todo => 
+        const { todos } = this.props;
+        const todoItems = todos.map(todo =>
             <TodoListItem key={todo.id} todo={todo} />);
-        return <ul>
-            {todoItems}
-        </ul>
+        return <div>
+            <ul>
+                {todoItems}
+            </ul>
+            <TodoForm receiveTodo={receiveTodo} />
+        </div>
     }
 }
 

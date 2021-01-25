fb5803d76d7ff06038073dd5e976b2142d3489de
Wayattearp <iason148348@gmail.com>
2021-01-24 19:04:29 -0500

    Implement drag and drop for inprogress todos

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import TodoListItem from './todo_list_item';
 import TodoForm from './todo_form'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 class TodoList extends React.Component {
 
     render() {
         const { todos, receiveTodo } = this.props;
         const doneTodoItems = [];
-        const InProgressTodoItems = [];
-        todos.map(todo => {
+        let InProgressTodoItems = [];
+        todos.map((todo) => {
             if (todo.done)
                 doneTodoItems.push(<TodoListItem key={todo.id} todo={todo} receiveTodo={receiveTodo} />);
             else {
@@ -17,27 +18,47 @@ class TodoList extends React.Component {
             }
         }
 
+
         );
         return <div>
+            <DragDropContext>
+                <Droppable droppableId="todoList">
+                    {(provided) => (
+                        <ul className="todoList" {...provided.droppableProps} ref={provided.innerRef}>
+                            <ReactCSSTransitionGroup
+                                transitionName='auto'
+                                transitionEnterTimeout={3000}
+                                transitionLeaveTimeout={500}>
+                                {InProgressTodoItems.map((todo, index) => {
+                                    return (
+                                        <Draggable key={todo.key} draggableId={todo.key} index={index}>
 
-            <ol className="todoList">
-                <ReactCSSTransitionGroup
-                    transitionName='auto'
-                    transitionEnterTimeout={3000}
-                    transitionLeaveTimeout={500}>
-                    {InProgressTodoItems}
-                </ReactCSSTransitionGroup>
-            </ol>
-            <TodoForm receiveTodo={receiveTodo} />
+                                            {(provided) => (
+                                                <span ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                    {todo}
+                                                </span>
+                                            )}
+                                        </Draggable>
+                                    )
+                                }
+                                )}
+                            </ReactCSSTransitionGroup>
 
-            <div className="todoList">
-                <ReactCSSTransitionGroup
-                    transitionName='auto'
-                    transitionEnterTimeout={3000}
-                    transitionLeaveTimeout={500}>
-                    {doneTodoItems}
-                </ReactCSSTransitionGroup>
-            </div>
+                        </ul>
+                    )}
+                </Droppable>
+
+                <TodoForm receiveTodo={receiveTodo} />
+
+                <div className="todoList">
+                    <ReactCSSTransitionGroup
+                        transitionName='auto'
+                        transitionEnterTimeout={3000}
+                        transitionLeaveTimeout={500}>
+                        {doneTodoItems}
+                    </ReactCSSTransitionGroup>
+                </div>
+            </DragDropContext>
         </div>
     }
 }

340c7abe9d4dfc935b5e14d3414265a497cf30e1
Wayattearp <iason148348@gmail.com>
2021-02-01 19:51:26 -0500

    Refactor todoListItem component into hook

--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,68 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoDetailViewContainer from './todo_detail_view_container';
 import { motion } from "framer-motion";
 
-class TodoListItem extends React.Component {
-    constructor(props) {
-        super(props)
-        this.toggleTodo = this.toggleTodo.bind(this);
-        this.state = {
-            detail: false
-        }
-        this.handleDetail = this.handleDetail.bind(this)
-    }
+function TodoListItem (props) {
 
-
-    toggleTodo(e) {
+    function toggleTodo(e) {
         e.preventDefault();
         const toggledTodo = Object.assign(
             {},
-            this.props.todo,
-            { done: !this.props.todo.done }
+            props.todo,
+            { done: !props.todo.done }
         );
 
-        this.props.receiveTodo(toggledTodo);
+        props.receiveTodo(toggledTodo);
     }
 
-    handleDetail() {
-        this.setState({ detail: !this.state.detail });
+    const [detail, handleDetail] = useState(false);
+    
+    let colors = ['e8efeb', 'adeecf', 'e8e8e4', 'ffd7ba', 'fae1dd', 'fdffb6', 'a9d6e5', 'fde2e4', 'e9edc9'];
+    let randomColor = colors[Math.floor(Math.random() * 9)];
+
+    const todo = props.todo;
+    const { title, done } = todo;
+
+    let todoView;
+
+    if (detail) {
+        todoView = <TodoDetailViewContainer todo={todo} color={randomColor} />
     }
 
-    render() {
-        let colors = ['e8efeb', 'adeecf', 'e8e8e4', 'ffd7ba', 'fae1dd', 'fdffb6', 'a9d6e5', 'fde2e4', 'e9edc9' ];
-        let randomColor = colors[Math.floor(Math.random() * 9)];
-
-        const todo = this.props.todo;
-        const { title, done } = todo;
-        let detail;
-
-        if (this.state.detail) {
-            detail = <TodoDetailViewContainer todo={todo} color={randomColor} />
-        }
-
-        return <motion.div
-            initial={{
-                scale: 0
+    return <motion.div
+        initial={{
+            scale: 0
+        }}
+        animate={{
+            scale: 1
+        }}
+        className="todoItemContainer">
+        <li className="todoItem"
+            style={{
+                background: '#' + randomColor
             }}
-            animate={{
-                scale: 1
-            }}
-            className="todoItemContainer">
-            <li className="todoItem"
-                style={{ 
-                    background: '#' + randomColor}}
-                onClick={this.handleDetail}>
-                {title}
-            </li>
-            <div>
-                {detail}
-            </div>
-            <button className={done ? "todoButtonDone" : "todoButtonInProgress"}
-                onClick={this.toggleTodo}>{done ? 'Done' : 'In Progress'}
-            </button>
+            onClick={() => handleDetail(!detail)}>
+            {title}
+        </li>
+        <div>
+            {todoView}
+        </div>
+        <button className={done ? "todoButtonDone" : "todoButtonInProgress"}
+            onClick={toggleTodo}>{done ? 'Done' : 'In Progress'}
+        </button>
 
-        </motion.div>
-    }
+    </motion.div>
 }
 
 export default TodoListItem
